c1f6abd9719aa357b00f743348d302017a1d4a20
scott <scottMAC@Admins-MacBook-Pro-6.local>
2015-03-06 18:39:55 -0600

    Added Login activity. Still looks ugly

--- app/src/main/java/mobi/idappthat/campusconnect/Activity/Login.java
+++ app/src/main/java/mobi/idappthat/campusconnect/Activity/Login.java
@@ -1,47 +1,25 @@
 package mobi.idappthat.campusconnect.Activity;
 
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.astuetz.PagerSlidingTabStrip;
-
-import mobi.idappthat.campusconnect.Adapter.TabsPagerAdapter;
-import mobi.idappthat.campusconnect.Fragment.PendingTransfersFragment;
 import mobi.idappthat.campusconnect.R;
 
-
-public class MainActivity extends ActionBarActivity {
+public class Login extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        getSupportActionBar().setElevation(0);
-
-        // Initialize the ViewPager and set an adapter
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager()));
-
-        // Bind the tabs to the ViewPager
-        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-        tabs.setViewPager(pager);
-
-        pager.setCurrentItem(1);
+        setContentView(R.layout.activity_login);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_after_login, menu);
         return true;
     }
 
