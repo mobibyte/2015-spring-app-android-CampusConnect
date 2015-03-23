bc862d7aa32c14cf98c6cd874fe78fdd08afa53c
Anthony Tatowicz <ajtatowicz@gmail.com>
2015-03-23 12:54:55 -0500

    Bluetooth scan added still needs improvement, but it works for a demo.

--- app/src/main/java/mobi/idappthat/campusconnect/Activity/EditProfileActivity.java
+++ app/src/main/java/mobi/idappthat/campusconnect/Activity/EditProfileActivity.java
@@ -1,11 +1,14 @@
 package mobi.idappthat.campusconnect.Activity;
 
+import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import mobi.idappthat.campusconnect.R;
@@ -23,30 +26,45 @@ public class EditProfileActivity extends ActionBarActivity implements View.OnCli
      * - Cameron
      * */
 
-
-    /**
-     * Yay we created a new fragment!!!
-     * Needs a container tho!!!!
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button button1 = (Button) findViewById(R.id.done);
-        Button button2 = (Button) findViewById(R.id.discard);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
+        final LayoutInflater inflater = (LayoutInflater) getActionBar().getThemedContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
 
-        Fragment editProfileFragment = new EditProfileFragment();
+        final View actionBarView = inflater.inflate(R.layout.menu_done_discard, null);
+
+        actionBarView.findViewById(R.id.actionbar_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+                        | ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(actionBarView,
+                new ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+
+        Button done = (Button) findViewById(R.id.done);
+        Button discard = (Button) findViewById(R.id.discard);
+        done.setOnClickListener(this);
+        discard.setOnClickListener(this);
+
+        /*Fragment editProfileFragment = new EditProfileFragment();
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.edit_profile_container, editProfileFragment)
                 .addToBackStack(null)
-                .commit();
+                .commit();*/
     }
 
-    //This is the method we need for our onclick implements and we can loop through buttons too!
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
